933854dc1fd686f616c549fe5958db8dcce782ed
BekKucharov <bek.jr.kucharov@gmail.com>
2021-04-02 01:26:59 +0500

    testCommit

---
+++ lesson 2.1.1/src/main/java/uz/pdp/springbootlesson1task1/SpringbootLesson1Task1Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.springbootlesson1task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLesson1Task1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLesson1Task1Application.class, args);
+    }
+
+}
